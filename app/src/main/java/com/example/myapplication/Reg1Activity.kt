b984d3ad3c5035febf0f214dec9cdff7cdf15af5
qas22 <stappy2022@gmail.com>
2022-05-21 22:43:06 +0900

    통합(홍의&주은&민우) 구글 통합 로그인은 빼놓은 상태

--- app/src/main/java/com/example/myapplication/Reg1Activity.kt
+++ app/src/main/java/com/example/myapplication/Reg1Activity.kt
@@ -1,7 +1,10 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_reg1.*
 import kotlinx.android.synthetic.main.activity_sub.*
 
 class Reg1Activity : AppCompatActivity() {
@@ -9,7 +12,11 @@ class Reg1Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reg1)
 
-        btnClose.setOnClickListener {
+        btnGoMainActivity1.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        btnClose2.setOnClickListener {
             finish()
         }
     }
